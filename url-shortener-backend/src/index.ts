70152a8b9e7aedd86425cd338f74978c7b53e237
Mauro Longano <mauro.longano@marconirovereto.it>
2025-03-01 16:48:30 +0100

    feat: add user registration endpoint with password hashing

--- url-shortener-backend/src/index.ts
+++ url-shortener-backend/src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { hashPassword } from '../../url-shortener-backend/src/lib/hashAndCompare';
 
 const app = new Hono()
 
 app.use('/api/*', cors())
+
 app.get('/', (c) => c.text('Hono!'))
+
 // Health check endpoint
 app.get('/health', (c) => {
   return c.json({
@@ -27,6 +30,51 @@ app.get('/api/v1/users', async (c) => {
   }
 
   return c.json(result.results, 200);
+});
+
+// Register user
+// POST /api/v1/auth/register
+// Request body: { email: string, password: string }
+// Response body: { success: boolean, results: any[] } | { success: boolean, message: string }
+type RegisterQuery = {
+  email: string;
+  password: string;
+};
+app.post('/api/v1/auth/register', async (c) => {
+  const { email, password } = await c.req.json<RegisterQuery>();
+
+  // Check payload
+  if (!email || !password) return c.json({
+    success: false,
+    results: []
+  }, 400);
+
+  const query = `
+      INSERT INTO users (email, password)
+      VALUES (?, ?)`;
+
+  const env = c.env as Env;
+
+  // Register user
+  try {
+    const hash = await hashPassword(password);
+    const results = await env.DB
+      .prepare(query)
+      .bind(email, hash)
+      .run();
+    return c.json({
+      success: true,
+      results: results
+    }, 201);
+  } catch (error: any) {
+    // Handle error if registration fails
+    console.error("Error registring user: ", error.message);
+    return c.json({
+      success: false,
+      message: error.message || "Internal server error",
+    }, 500);
+  }
+
 });
 
 export default app
