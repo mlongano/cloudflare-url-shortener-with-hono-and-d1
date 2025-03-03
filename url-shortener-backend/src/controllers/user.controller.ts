6707b0dd2348193311adacd5b2bebccdd9d8c869
Mauro Longano <mauro.longano@marconirovereto.it>
2025-03-04 00:41:25 +0100

    feat: add user profile retrieval endpoint with authentication

--- url-shortener-backend/src/controllers/user.controller.ts
+++ url-shortener-backend/src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Context } from 'hono';
-
+import { AuthUserId } from '../types';
 
 export const getAllUsers = async (c: Context<{ Bindings: Env }>) => {
   const env = c.env as Env;
@@ -15,4 +15,26 @@ export const getAllUsers = async (c: Context<{ Bindings: Env }>) => {
   }
 
   return c.json(result.results, 200);
+};
+
+export const getProfile = async (c: Context<{ Bindings: Env, Variables: { user: AuthUserId } }>) => {
+
+  const id = 1;
+
+  const results = await c.env.DB.prepare(
+    `SELECT * FROM users LEFT OUTER JOIN subscriptions ON users.id = subscriptions.user_id LEFT OUTER JOIN urls ON users.id = urls.user_id WHERE users.id=?;`,
+  )
+    .bind(id)
+    .all();
+  if (!results) {
+    return c.json({
+      success: false,
+      results: []
+    }, 404);
+  }
+
+  return c.json({
+    success: true,
+    results
+  }, 200);
 };
--- url-shortener-backend/src/routes/user.routes.ts
+++ url-shortener-backend/src/routes/user.routes.ts
@@ -1,8 +1,13 @@
 import { Hono } from 'hono';
-import { getAllUsers } from '../controllers/user.controller';
+import { getAllUsers, getProfile } from '../controllers/user.controller';
+import { authenticate, jwtAuthenticate } from '../middleware/auth.middleware';
 
 const userRouter = new Hono<{ Bindings: Env }>();
 
 userRouter.get('/', getAllUsers);
+userRouter.get('/me',
+  jwtAuthenticate(),
+  authenticate,
+  getProfile);
 
 export default userRouter;
