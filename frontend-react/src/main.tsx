b6f171a12683eab92bf1ddbb27c31476076f58e8
Mauro Longano <mauro.longano@marconirovereto.it>
2025-04-06 09:57:16 +0200

    fix(fe): fix  import of BrowserRouter in main.tsx

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Import
 const queryClient = new QueryClient();
 
